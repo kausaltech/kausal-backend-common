d459455d09f752d78bf8e590cb861617226ed764
Juha Yrjölä <juha.yrjola@iki.fi>
2024-06-17 14:05:28 +0300

    Set Kube cluster information as Sentry tags

--- logging/http.py
+++ logging/http.py
@@ -22,5 +22,11 @@ def start_request(request: HttpRequest):
     cluster_name = os.getenv('CLUSTER_NAME', None)
     if cluster_name:
         sentry_sdk.set_tag('cluster', cluster_name)
+    node_name = os.getenv("NODE_NAME", None)
+    if node_name:
+        sentry_sdk.set_tag("cluster_node", node_name)
+    pod_name = os.getenv("POD_NAME", None)
+    if pod_name:
+        sentry_sdk.set_tag("cluster_pod", pod_name)
     with logger.contextualize(**{"correlation_id": request.correlation_id}):
         yield
