e3bb515c1fa3c8448f1e60358548e3f8b9087848
Juha Yrjölä <juha.yrjola@iki.fi>
2024-07-19 16:33:00 +0300

    Add human-readable name for the UUID field

--- models/uuid.py
+++ models/uuid.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from uuid import uuid4
 
 
 class UUIDIdentifiedModel(models.Model):
-    uuid = models.UUIDField(editable=False, default=uuid4, unique=True)  # pyright: ignore
+    uuid = models.UUIDField(_('UUID'), editable=False, default=uuid4, unique=True)  # pyright: ignore
 
     class Meta:
         abstract = True
