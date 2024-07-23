bc252f40f9484d41f9ef8df1e92bf9564de4c26f
Juha Yrjölä <juha.yrjola@iki.fi>
2024-07-23 18:41:45 +0300

    Add validation methods for uuids

--- models/uuid.py
+++ models/uuid.py
@@ -1,10 +1,35 @@
+import re
+from uuid import uuid4
+
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from uuid import uuid4
+from django.contrib.postgres.functions import RandomUUID
+
+
+UUID_PATTERN = re.compile(r'^[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}$', re.IGNORECASE)
+
+
+def is_uuid(s: str):
+    return bool(UUID_PATTERN.match(s))
+
+
+def is_valid_pk_or_uuid(s: str):
+    if is_uuid(s):
+        return True
+    if s.isnumeric():
+        return True
+    return False
+
+
+def query_pk_or_uuid(s: str):
+    if is_uuid(s):
+        return models.Q(uuid=s)
+    else:
+        return models.Q(id=int(s))
 
 
 class UUIDIdentifiedModel(models.Model):
-    uuid = models.UUIDField(_('UUID'), editable=False, default=uuid4, unique=True)  # pyright: ignore
+    uuid = models.UUIDField(_('UUID'), editable=False, db_default=RandomUUID(), unique=True)  # pyright: ignore
 
     class Meta:
         abstract = True
