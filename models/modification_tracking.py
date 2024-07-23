a38760b7552400cc166b4cd9d6aa0df513937411
Juha Yrjölä <juha.yrjola@iki.fi>
2024-07-23 10:20:19 +0300

    Add abstract model class for simple modification tracking

---
+++ models/modification_tracking.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+if TYPE_CHECKING:
+    from users.models import User
+
+
+class UserModifiableModel(models.Model):
+    """
+    An abstract base model that includes fields for tracking creation and modification.
+
+    This model provides common fields for tracking when an object was created,
+    who created it, when it was last modified, and who last modified it.
+    It is designed to be inherited by other models that require this functionality.
+
+    Fields:
+        modified_at (DateTimeField): The timestamp of the last modification.
+        created_at (DateTimeField): The timestamp of when the object was created.
+        created_by (ForeignKey): The user who created the object.
+        last_modified_by (ForeignKey): The user who last modified the object.
+    """
+
+    created_at = models.DateTimeField(verbose_name=_("Created at"), auto_now_add=True, editable=False)
+    created_by: models.ForeignKey[User | None, User | None] = models.ForeignKey(  # pyright: ignore
+        'users.User',
+        verbose_name=_("Last modified by"),
+        on_delete=models.SET_NULL,
+        null=True,
+        editable=False,
+        related_name='+',
+    )
+    last_modified_at = models.DateTimeField(verbose_name=_("Last modified at"), auto_now=True, editable=False)
+    last_modified_by = models.ForeignKey(
+        'users.User',
+        verbose_name=_("Last modified by"),
+        on_delete=models.SET_NULL,
+        null=True,
+        editable=False,
+        related_name='+',
+    )
+
+    class Meta:
+        abstract = True
