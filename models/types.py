b2863646d7acb0279acb2151b04f671ce48405b9
Juha Yrjölä <juha.yrjola@iki.fi>
2024-08-07 15:18:37 +0300

    Add a generics-compatible version of JSONField

--- models/types.py
+++ models/types.py
@@ -1,6 +1,13 @@
 from typing import TYPE_CHECKING
 
-from django.db.models import ForeignKey as DjangoForeignKey, ManyToManyField as DjangoManyToManyField, Model
+from django.db.models import (
+    ForeignKey as DjangoForeignKey,
+    JSONField,  # noqa: F401
+    ManyToManyField as DjangoManyToManyField,
+    Model,
+)
+
+from ..typings.monkey import monkeypatch_generic_support
 
 type NullableModel[M: Model] = M | None
 
@@ -8,6 +15,9 @@ type ForeignKey[M: Model] = DjangoForeignKey[M, M]  # pyright: ignore
 type NullableFK[M: Model] = DjangoForeignKey[M | None, M | None]  # pyright: ignore
 type ManyToManyField[To: Model, Through: Model] = DjangoManyToManyField[To, Through]
 
+
 if TYPE_CHECKING:
     from django.db.models.fields.related_descriptors import RelatedManager  # pyright: ignore
     from django.db.models.manager import RelatedManager  # type: ignore  # noqa: F811
+else:
+    monkeypatch_generic_support(JSONField)
