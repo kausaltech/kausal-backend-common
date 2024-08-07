af5e4ffaf3c80edfee1f9cf42bfcd2f9139e22b3
Juha Yrjölä <juha.yrjola@iki.fi>
2024-08-07 15:17:52 +0300

    Allow generics monkeypatching to be invoked on user-supplied classes

--- typings/monkey.py
+++ typings/monkey.py
@@ -1,6 +1,26 @@
-def monkeypatch_generic_support():
+from typing import Sequence
+
+
+def _monkeypatch_one_class(kls: type) -> None:
+    kls.__class_getitem__ = classmethod(lambda cls, *args, **kwargs: cls)  # type: ignore  # noqa: ARG005
+
+
+def _monkeypath_init() -> None:
     import django_stubs_ext
+    from django.db.models.fields.json import JSONField
+    from modelcluster.fields import ParentalKey
     from wagtail.admin.viewsets.model import ModelViewSet
     from wagtail.permission_policies.base import ModelPermissionPolicy
 
-    django_stubs_ext.monkeypatch([ModelViewSet, ModelPermissionPolicy])
+    django_stubs_ext.monkeypatch([ModelViewSet, ModelPermissionPolicy, ParentalKey, JSONField])
+
+
+def monkeypatch_generic_support(kls: type | Sequence[type] | None = None):
+    if kls is None:
+        _monkeypath_init()
+        return
+    if isinstance(kls, type):
+        kls = [kls]
+    for k in kls:
+        _monkeypatch_one_class(k)
+
