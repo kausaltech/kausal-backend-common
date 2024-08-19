34b7381cd3aeed03909f8ef7fb53de76565e2303
Juha Yrjölä <juha.yrjola@iki.fi>
2024-08-19 23:01:35 +0300

    Implement general contextvar with type-safe subclass access

---
+++ context/single.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from contextlib import contextmanager
+from contextvars import ContextVar
+from dataclasses import dataclass, field
+
+
+@dataclass
+class SubclassContext[ValueT]:
+    container_type: type[ValueT]
+    context_name: str
+
+    _context_var: ContextVar[ValueT] = field(init=False)
+
+    def __post_init__(self):
+        self._context_var = ContextVar(self.context_name)
+
+    @contextmanager
+    def activate(self, value: ValueT):
+        """
+        Set this context as the currently active one.
+
+        When the runtime context finishes, the previous context (if any) will be restored.
+        """
+        token = self._context_var.set(value)
+        try:
+            yield
+        finally:
+            self._context_var.reset(token)
+
+    def get(self) -> ValueT:
+        return self._context_var.get()
+
+    def get_as_type[SubClassT](self, klass: type[SubClassT]) -> SubClassT:
+        val = self.get()
+        if not isinstance(val, klass):
+            raise TypeError("Context value type is '%s'; expected '%s'" % (type(val), klass))
+        return val
