6b4060ccd83fbec8dcda41156dab63157e2f3b56
Juha Yrjölä <juha.yrjola@iki.fi>
2024-08-21 14:48:03 +0300

    Fix mypy-pre-push.sh

--- development/git-hooks/mypy-pre-push.sh
+++ development/git-hooks/mypy-pre-push.sh
@@ -5,12 +5,21 @@ set -euo pipefail
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 # Filter the files and store the result
-filtered_files=$(printf '%s\n' "$@" | "${SCRIPT_DIR}/filter-mypy-ignores.py")
+mapfile -t filtered_files < <(printf '%s\n' "$@" | "${SCRIPT_DIR}/filter-mypy-ignores.py")
 
 # Check if there are any files left after filtering
-if [ -n "$filtered_files" ]; then
+if [ ${#filtered_files[@]} -gt 0 ]; then
     # Run mypy on the filtered files
-    echo "$filtered_files" | tr '\n' '\0' | xargs -0 mypy | exec mypy-baseline filter --allow-unsynced
+    set +e
+    mypy_output=$(mypy "${filtered_files[@]}" )
+    mypy_exit_code=$?
+    if [ $mypy_exit_code -ne 0 ] && [ $mypy_exit_code -ne 1 ]; then
+        echo "mypy crashed with exit code $mypy_exit_code. Exiting."
+        echo "$mypy_output"
+        exit $mypy_exit_code
+    fi
+    set -e
+    echo "$mypy_output" | exec mypy-baseline filter --allow-unsynced --hide-stats
 else
     echo "No files to check after filtering. Skipping mypy."
 fi
