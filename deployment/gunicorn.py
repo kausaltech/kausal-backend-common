2aae15f45d57e98f578a20a6defc220e0d8cd45c
Juha Yrjölä <juha.yrjola@iki.fi>
2024-09-03 00:56:04 +0300

    Update gunicorn deployment

--- deployment/gunicorn.py
+++ deployment/gunicorn.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+import contextlib
+import gc
+import os
+from threading import Thread
 from typing import TYPE_CHECKING
 
 from loguru import logger
@@ -14,16 +18,44 @@ if TYPE_CHECKING:
 gunicorn_log = logger.bind(name='gunicorn.start')
 
 
+def pre_import():
+    """Import import some of the larger packages before forking."""
+
+    with contextlib.suppress(ImportError):
+        import pandas as pd  # noqa: F401
+
+    with contextlib.suppress(ImportError):
+        import polars as pl  # noqa: F401
+
+    with contextlib.suppress(ImportError):
+        from nodes.units import unit_registry  # noqa: F401
+
+    gunicorn_log.info("Additional imports completed")
+
+
 def when_ready(server: Arbiter):
     from kausal_common.deployment import run_deployment_checks
 
     gunicorn_log.info('Server ready')
 
     settings = server.cfg.settings
-    if settings['preload_app'].value:
-        gunicorn_log.info('Running deployment checks')
-        run_deployment_checks()
-        gunicorn_log.info('Deployment checks completed')
+    if not settings['preload_app'].value:
+        return
+
+    gunicorn_log.info('Running deployment checks')
+    run_deployment_checks()
+    gunicorn_log.info('Deployment checks completed')
+
+    pre_import()
+
+
+def post_worker_init(worker: Worker):
+    from django.core.cache import close_caches
+    from django.db import connections
+
+    for conn in connections.all(initialized_only=True):
+        conn.close()
+    close_caches()
 
 
 def post_fork(server: Arbiter, worker: Worker):
@@ -35,4 +67,5 @@ def get_gunicorn_hooks():
     return dict(
         when_ready=when_ready,
         post_fork=post_fork,
+        post_worker_init=post_worker_init,
     )
