fc100c2218b0987fff49ee51c98c08813e6b5128
Timo Tuominen <tituomin@gmail.com>
2024-09-12 10:29:06 +0300

    Force exclusion of eg. migrations in ruff pre-commit hook
    Since migrations were passed explicitly in the command line,
    they were being checked in the hook

--- development/git-hooks/ruff-pre-commit.sh
+++ development/git-hooks/ruff-pre-commit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Check against the changed files
-exec ruff check --output-format concise "$@" | ondivi --baseline HEAD
+exec ruff check --force-exclude --output-format concise "$@" | ondivi --baseline HEAD
