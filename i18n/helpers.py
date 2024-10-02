dfcd027bdcf7a9ce4c4eb8578467584fbec92d30
Matias Wargelin <matias.wargelin@kausal.tech>
2024-10-02 12:43:54 +0300

    Add function for converting language codes

--- i18n/helpers.py
+++ i18n/helpers.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+import re
+from typing import TYPE_CHECKING, Literal
 
 from django.conf import settings
 from modeltrans.translator import get_i18n_field
@@ -9,6 +10,8 @@ from modeltrans.utils import get_instance_field_value
 if TYPE_CHECKING:
     from django.db import models
     from modeltrans.fields import TranslationField
+
+LANGUAGE_CODE_REGEXP = re.compile(r'^([a-zA-Z]{2})(?:[_-]{1}([a-zA-Z]{2}))?$')
 
 
 def get_language_from_default_language_field(
@@ -31,3 +34,46 @@ def get_language_from_default_language_field(
     else:
         raise ValueError('Invalid default_language for', instance, default_language)
     return default_language
+
+def convert_language_code(language_code: str, output_format: Literal['kausal', 'django', 'modeltrans', 'next.js']) -> str:
+    """
+    Convert given language code to wanted output format.
+
+    Args:
+        language_code (str): The language code to convert.
+        output_format (str): The output format to which to convert the language code to. The formats are:
+            'kausal': The format to use internally. (e.g. 'es-US')
+            'django': The format used by Django, e.g. django.utils.translation.get_language. ('es-us')
+            'modeltrans': The format used by django-modeltrans. ('es_us')
+            'next.js': The format used by next.js. ('es-US')
+
+    Returns:
+        Given language code converted to wanted format as a string.
+
+    Raises:
+        ValueError: If language_code or output_format are invalid.
+
+    """
+    regex_match = LANGUAGE_CODE_REGEXP.match(language_code)
+    if not regex_match:
+        error_message = f"'{language_code}' is not a valid language code."
+        raise ValueError(error_message)
+
+    language, region = regex_match.groups()
+    match output_format:
+        case "kausal" | "next.js":
+            result = language.lower()
+            result += f"-{region.upper()}" if region else ""
+            return result
+        case "django":
+            result = language.lower()
+            result += f"-{region.lower()}" if region else ""
+            return result
+        case "modeltrans":
+            result = language.lower()
+            result += f"_{region.lower()}" if region else ""
+            return result
+        case _:
+            format_options = ["kausal", "django", "modeltrans", "next.js"]
+            error_message = f"'{output_format}' is not a valid language code format. Valid formats are {format_options}"
+            raise ValueError(error_message)
---
+++ tests/test_i18n.py
@@ -0,0 +1,83 @@
+from __future__ import annotations
+
+import pytest
+
+from kausal_common.i18n import helpers
+
+# This is useless for these tests, but is needed for the moment so that these tests can be run in Paths environment
+pytestmark = pytest.mark.django_db
+
+
+@pytest.mark.parametrize(
+    ('language_code', 'is_valid'),
+    [
+        ('', False),
+        ('fin', False),
+        ('eng-us', False),
+        ('en-usa', False),
+        ('en--us', False),
+        ('en-_us', False),
+        ('en-u', False),
+        ('enus', False),
+        ('en', True),
+        ('EN', True),
+        ('en-us', True),
+        ('en-US', True),
+        ('en_us', True),
+        ('en_US', True),
+        ('EN-us', True),
+        ('EN-US', True),
+        ('EN_us', True),
+        ('EN_US', True),
+    ],
+)
+def test_convert_language_code_input_validation(language_code, is_valid):
+    """Test that trying to convert invalid language code raises an error."""
+    if is_valid:
+        # Valid codes simply pass
+        helpers.convert_language_code(language_code, 'kausal')
+    else:
+        # Invalid codes raise an error
+        with pytest.raises(ValueError, match=f"'{language_code}' is not a valid language code."):
+            helpers.convert_language_code(language_code, 'kausal')
+
+
+@pytest.mark.parametrize(
+    ('output_format', 'is_valid'),
+    [
+        ('', False),
+        ('invalid', False),
+        ('kausal', True),
+        ('django', True),
+        ('modeltrans', True),
+        ('next.js', True),
+    ],
+)
+def test_convert_language_code_format_validation(output_format, is_valid):
+    """Test that trying to convert a language code to an invalid language code format raises an error."""
+    if is_valid:
+        # Valid formats simply pass
+        helpers.convert_language_code('en', output_format)
+    else:
+        # Invalid formats raise an error
+        with pytest.raises(ValueError, match=f"'{output_format}' is not a valid language code format. Valid formats are"):
+            helpers.convert_language_code('en', output_format)
+
+
+@pytest.mark.parametrize(
+    ('language_code', 'output_format', 'wanted_result'),
+    [
+        ('EN', 'kausal', 'en'),
+        ('EN_us', 'kausal', 'en-US'),
+        ('FI', 'django', 'fi'),
+        ('FI_SV', 'django', 'fi-sv'),
+        ('FI', 'modeltrans', 'fi'),
+        ('FI-SV', 'modeltrans', 'fi_sv'),
+        ('EN', 'next.js', 'en'),
+        ('EN_us', 'next.js', 'en-US'),
+    ],
+)
+def test_convert_language_code(language_code, output_format, wanted_result):
+    """Test converting language code to wanted format."""
+    result = helpers.convert_language_code(language_code, output_format)
+    assert result == wanted_result
