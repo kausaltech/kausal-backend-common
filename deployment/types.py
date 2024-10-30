8ab49e158bfb54fe0f2330b9977bfb98a7e235c6
Juha Yrjölä <juha.yrjola@iki.fi>
2024-10-30 23:21:07 +0200

    Some helpers for determining deployment environment

--- deployment/types.py
+++ deployment/types.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
 from enum import Enum
+from functools import cache
+import os
+from warnings import warn
 
 
 class DeploymentEnvironmentType(Enum):
@@ -8,8 +11,25 @@ class DeploymentEnvironmentType(Enum):
     STAGING = 'staging'
     TESTING = 'testing'
     WIP = 'wip'
-
+    DEV = 'development'
 
 def is_production_deployment() -> bool:
-    from django.conf import settings
-    return settings.DEPLOYMENT_TYPE == DeploymentEnvironmentType.PRODUCTION
+    return get_deployment_environment() == DeploymentEnvironmentType.PRODUCTION
+
+def is_development_environment() -> bool:
+    return get_deployment_environment() == DeploymentEnvironmentType.DEV
+
+@cache
+def get_deployment_environment() -> DeploymentEnvironmentType:
+    dt_val: str
+    try:
+        from django.conf import settings
+        dt_val = settings.DEPLOYMENT_TYPE
+    except Exception:
+        dt_val = os.getenv('DEPLOYMENT_TYPE') or 'development'
+    try:
+        dt = DeploymentEnvironmentType(value=dt_val)
+    except ValueError:
+        warn("Invalid deployment environment type: %s; defaulting to 'development'", stacklevel=1)
+        return DeploymentEnvironmentType.DEV
+    return dt
