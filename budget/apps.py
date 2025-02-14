4c3189b67a6245b269b503e4607f1e13a4d6d0ed
Jan-Erik S. <6697396+Bhavatu@users.noreply.github.com>
2025-02-14 14:17:36 +0200

    add migrations and config

--- budget/apps.py
+++ budget/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class BudgetConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'budget'
+    name = 'kausal_common.budget'
+    label = 'budget'
