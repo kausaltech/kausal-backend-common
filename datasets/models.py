6d06771e2f31ff44360ee125e8bdaaa76f999b0f
Timo Tuominen <tituomin@gmail.com>
2025-03-12 10:26:42 +0200

    Remove unit from schema

--- datasets/models.py
+++ datasets/models.py
@@ -148,7 +148,6 @@ class DatasetSchema(ClusterableModel):
         default=TimeResolution.YEARLY,
         help_text=_('Time resolution of the time stamps of data points in this dataset'),
     )
-    unit = models.CharField(max_length=100, blank=True, verbose_name=_('unit'))
     name = models.CharField(max_length=100, blank=False, verbose_name=_('name'))
     description = models.TextField(blank=True)
     start_date = models.DateField(
@@ -158,8 +157,7 @@ class DatasetSchema(ClusterableModel):
         help_text=_("First applicable date for datapoints in these datasets"),
     )
 
-    i18n = TranslationField(fields=['unit', 'name'])
-    unit_i18n: str
+    i18n = TranslationField(fields=['name'])
     name_i18n: str
 
     objects: models.Manager[DatasetSchema]
@@ -176,11 +174,6 @@ class DatasetSchema(ClusterableModel):
             'description',
             heading=_("Description"),
             help_text=_("Description of the content and use of the dataset"),
-        ),
-        FieldPanel(
-            'unit',
-            heading=_("Unit"),
-            help_text=_("Unit of the dataset schema"),
         ),
         FieldPanel(
             'time_resolution',
