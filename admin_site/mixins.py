2e6a69e2c1ed72971363daf8d347a86093f45e8c
Matias Wargelin <matias.wargelin@kausal.tech>
2025-03-17 14:40:50 +0200

    Update typings for HideSnippetsFromBreadCrumbsMixin

--- admin_site/mixins.py
+++ admin_site/mixins.py
@@ -1,11 +1,18 @@
 """Mixins for Wagtail admin UI views."""
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+    from typings.wagtail.admin.views.generic.base import BreadcrumbItem
+
 
 class HideSnippetsFromBreadcrumbsMixin:
     """Hide access to snippets index view from view breadcrumbs."""
 
-    def get_breadcrumbs_items(self) -> list[dict]:
+    def get_breadcrumbs_items(self) -> Sequence[BreadcrumbItem]:
         breadcrumb_items = super().get_breadcrumbs_items()  # type: ignore
         breadcrumb_items = [item for item in breadcrumb_items if item['label'] != 'Snippets']
         return breadcrumb_items
