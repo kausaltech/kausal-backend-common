63849a5af0e757b5aac491e35e6fdfdcb0f161f1
Bernhard Bliem <bernhard@bliem.xyz>
2025-03-26 17:09:06 +0100

    Implement mutations for registering and deleting users
    These are protected by (1) an environment variable, and (2) a check
    whether the deployment is a development environment.
    
    This relies on every project using kausal-backend-common to define
    `User` in `users.models` and `UserType` in `users.schema`.

--- __init__.py
+++ __init__.py
@@ -2,8 +2,15 @@ from __future__ import annotations
 
 from .mail.env import ENV_SCHEMA as MAIL_ENV_SCHEMA
 
+# Used in ENV_SCHEMA -- all of these environment variables will be exposed as variables in `settings`
+EXPOSED_SETTINGS_ENV_SCHEMA = dict(
+    DANGEROUSLY_ENABLE_DELETE_USER_MUTATION=(bool, False),
+    DANGEROUSLY_ENABLE_REGISTER_USER_MUTATION=(bool, False),
+)
+
 # kwargs to be added to arguments to django-environ's Env.__init__()
 ENV_SCHEMA = dict(
+    **EXPOSED_SETTINGS_ENV_SCHEMA,
     **MAIL_ENV_SCHEMA,
 )
 
@@ -12,3 +19,8 @@ def register_settings(settings: dict):
     from .mail.settings import register_settings as register_mail_settings
     register_mail_settings(settings)
     settings['FORMS_URLFIELD_ASSUME_HTTPS'] = True
+
+    # Expose variables from EXPOSED_SETTINGS_ENV_SCHEMA to `settings`
+    env = settings['env']
+    for var in EXPOSED_SETTINGS_ENV_SCHEMA.keys():
+        settings[var] = env(var)
