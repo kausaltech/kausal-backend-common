e69fcd5090046537aa013973620352948e5885d8
Juha Yrjölä <juha.yrjola@iki.fi>
2025-05-16 17:44:04 +0300

    Add some more (incomplete) type stubs

---
+++ typings/wagtail/admin/widgets/auto_height_text.pyi
@@ -0,0 +1,6 @@
+from django.forms import widgets
+
+from _typeshed import Incomplete
+
+class AdminAutoHeightTextInput(widgets.Textarea):
+    def __init__(self, attrs: Incomplete | None = None) -> None: ...
