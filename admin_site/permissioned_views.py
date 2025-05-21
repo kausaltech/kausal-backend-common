62f783d1ca9f56cd6a0912799efa515c7b8f0245
Timo Tuominen <tituomin@gmail.com>
2025-05-21 22:16:42 +0300

    Add some support for Snippet View(Set)s which use permission policy
    Any admin views that need to enforce permission policies have
    to subclass these views

---
+++ admin_site/permissioned_views.py
@@ -0,0 +1,113 @@
+from __future__ import annotations
+
+from functools import cached_property
+from typing import TYPE_CHECKING, Any, ClassVar, Generic, cast
+from typing_extensions import TypeVar
+
+from django.contrib.auth.models import AbstractBaseUser, AnonymousUser
+from django.core.exceptions import FieldDoesNotExist
+from django.db.models import Model, QuerySet
+from django.forms import BaseModelForm
+from wagtail.admin.forms.models import WagtailAdminModelForm
+from wagtail.snippets.views.chooser import ChooseResultsView, ChooseView, SnippetChooserViewSet
+from wagtail.snippets.views.snippets import (
+    CreateView,
+    DeleteView,
+    EditView,
+    SnippetViewSet,
+)
+
+from kausal_common.admin_site.mixins import HideSnippetsFromBreadcrumbsMixin
+from kausal_common.models.permission_policy import ModelPermissionPolicy
+from kausal_common.models.permissions import PermissionedModel
+
+from users.models import User
+
+if TYPE_CHECKING:
+    from django.http import HttpRequest
+    from wagtail.admin.panels.group import ObjectList
+    from wagtail.permission_policies.base import BasePermissionPolicy
+
+_ModelT = TypeVar('_ModelT', bound=Model, default=Model, covariant=True)  # noqa: PLC0105
+_QS = TypeVar('_QS', bound=QuerySet[Any, Any], default=QuerySet[_ModelT, _ModelT])
+
+
+class _ModelForm[M: Model](WagtailAdminModelForm[M, User]):
+    pass
+
+_FormT = TypeVar('_FormT', bound=BaseModelForm[Any], default=_ModelForm[_ModelT])
+
+
+def user_has_permission(
+    permission_policy: BasePermissionPolicy,
+    user: AbstractBaseUser | AnonymousUser,
+    permission: str,
+    obj: Model,
+    context: HttpRequest | None = None,
+) -> bool:
+    assert isinstance(permission_policy, ModelPermissionPolicy)
+    if isinstance(user, AnonymousUser):
+        return False
+    if permission == 'add':
+        return permission_policy.user_can_create(user, context=context)
+    return permission_policy.user_has_permission_for_instance(
+        user, permission, obj
+    )
+
+
+class PermissionedEditView(HideSnippetsFromBreadcrumbsMixin, EditView[_ModelT, _FormT]):
+    def user_has_permission(self, permission):
+        return user_has_permission(
+            self.permission_policy,
+            self.request.user,
+            permission,
+            self.object
+        )
+
+    def get_editing_sessions(self):
+        return None
+
+
+class PermissionedDeleteView(DeleteView[_ModelT, _FormT]):
+    def user_has_permission(self, permission):
+        return user_has_permission(
+            self.permission_policy,
+            self.request.user,
+            permission,
+            self.object
+        )
+
+
+class PermissionedCreateView(HideSnippetsFromBreadcrumbsMixin, CreateView[_ModelT, _FormT]):
+    def user_has_permission(self, permission):
+        return user_has_permission(
+            self.permission_policy,
+            self.request.user,
+            permission,
+            None,
+            context=self.request,
+        )
+
+
+class PermissionedViewSet(Generic[_ModelT, _QS, _FormT], SnippetViewSet[_ModelT, _FormT]):
+    add_view_class: ClassVar = PermissionedCreateView[_ModelT, _FormT]
+    edit_view_class: ClassVar = PermissionedEditView[_ModelT, _FormT]
+    # index_view_class: ClassVar = PathsIndexView[_ModelT, _QS]
+    # delete_view_class: ClassVar = PathsDeleteView
+    # usage_view_class: ClassVar = PathsUsageView
+
+    add_to_admin_menu = True
+
+    @cached_property
+    def url_prefix(self) -> str:
+        return f"{self.app_label}/{self.model_name}"
+
+    @cached_property
+    def url_namespace(self) -> str:
+        return f"{self.app_label}_{self.model_name}"
+
+    @property
+    def permission_policy(self):
+        if issubclass(self.model, PermissionedModel):
+            return self.model.permission_policy()
+        return super().permission_policy
