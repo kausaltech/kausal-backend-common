0439ee9c0dccb74d26485fc52b2732982655e5d8
Bernhard Bliem <bernhard@bliem.xyz>
2025-07-17 17:23:34 +0200

    Revamp person chooser

--- people/chooser.py
+++ people/chooser.py
@@ -1,100 +1,35 @@
-from django.forms.models import modelform_factory
-from django.utils.translation import gettext_lazy as _
-from wagtail import hooks
-from wagtail.search.backends import get_search_backend
+from __future__ import annotations
 
-from dal import autocomplete
-from generic_chooser.views import ModelChooserCreateTabMixin, ModelChooserMixin, ModelChooserViewSet
-from generic_chooser.widgets import AdminChooser
-
-from kausal_common.const import IS_PATHS, IS_WATCH
-
-from people.models import Person
+from django.contrib.admin.utils import quote
+from django.urls import reverse
+from wagtail.admin.ui.tables import Column, TitleColumn
 
 
-class PersonChooserMixin(ModelChooserMixin):
-    def get_unfiltered_object_list(self):
-        objects = self.model.objects.all()
-        if self.order_by:
-            objects = objects.order_by('last_name', 'first_name')
-        return objects
+class BasePersonChooseViewMixin:
+    """
+    Base for mixins for subclasses of Wagtail's `BaseChooseView`.
 
-    def get_object_list(self, search_term=None, **kwargs):
-        if IS_WATCH:
-            plan = self.request.user.get_active_admin_plan()
-            object_list = self.get_unfiltered_object_list().available_for_plan(plan)
-        elif IS_PATHS:
-            admin_instance = self.request.user.get_active_instance()
-            object_list = self.get_unfiltered_object_list().available_for_instance(admin_instance)
-        else:
-            object_list = self.model.objects.none()
-        # Workaround to prevent Wagtail from looking for `path` (from Organization) in the search fields for Person
-        object_list = self.model.objects.filter(id__in=object_list)
+    You'll want to override some additional methods of that view, such as `get_object_list()`.
+    """
 
-        if search_term:
-            search_backend = get_search_backend()
-            object_list = search_backend.autocomplete(search_term, object_list)
+    chosen_url_name: str
 
-        return object_list
+    ordering = ('last_name', 'first_name')
 
-    def get_row_data(self, item):
-        avatar_url = item.get_avatar_url(self.request, '50x50')
-        return {
-            'choose_url': self.get_chosen_url(item),
-            'name': self.get_object_string(item),
-            'title': item.title,
-            'organization': item.organization,
-            'avatar_url': avatar_url,
+    @property
+    def columns(self):
+        link_kwargs = {
+            'get_url': (
+                lambda obj: self.append_preserved_url_parameters(  # type: ignore
+                    reverse(self.chosen_url_name, args=(quote(obj.pk),))
+                )
+            ),
+            'link_attrs': {'data-chooser-modal-choice': True},
         }
-
-    def get_results_template(self):
-        return 'people/chooser_results.html'
-
-
-class PersonModelChooserCreateTabMixin(ModelChooserCreateTabMixin):
-    create_tab_label = _("Create new")
-
-    def get_initial(self):
-        if IS_WATCH:
-            plan = self.request.user.get_active_admin_plan()
-            return {'organization': plan.organization}
-        elif IS_PATHS:
-            admin_instance = self.request.user.get_active_instance()
-            return {'organization': admin_instance.organization}
-        return {}
-
-    def get_form_class(self):
-        if self.form_class:
-            return self.form_class
-
-        organization_widget = autocomplete.ModelSelect2(url='organization-autocomplete')
-
-        self.form_class = modelform_factory(self.model, fields=self.fields, widgets=dict(
-            organization=organization_widget,
-        ))
-        return self.form_class
-
-
-class PersonChooserViewSet(ModelChooserViewSet):
-    chooser_mixin_class = PersonChooserMixin
-    create_tab_mixin_class = PersonModelChooserCreateTabMixin
-
-    icon = 'user'
-    model = Person
-    page_title = _("Choose person")
-    per_page = 10
-    order_by = ('last_name', 'first_name')
-    fields = ['first_name', 'last_name', 'email', 'title', 'organization']
-
-
-class PersonChooser(AdminChooser):
-    choose_one_text = _('Choose a person')
-    choose_another_text = _('Choose another person')
-    link_to_chosen_text = _('Edit this person')
-    model = Person
-    choose_modal_url_name = 'person_chooser:choose'
-
-
-@hooks.register('register_admin_viewset')
-def register_person_chooser_viewset():
-    return PersonChooserViewSet('person_chooser', url_prefix='person-chooser')
+        return [
+            TitleColumn('first_name', **link_kwargs),
+            TitleColumn('last_name', **link_kwargs),
+            TitleColumn('email', **link_kwargs),
+            Column('title'),
+            Column('organization'),
+        ]
