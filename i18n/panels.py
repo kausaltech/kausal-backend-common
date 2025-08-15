900fdbe354fdfce4befd869231f0a7848dad3c92
Timo Tuominen <tituomin@gmail.com>
2025-08-15 14:58:50 +0300

    Port Watch improvements to common i18n panels

--- i18n/panels.py
+++ i18n/panels.py
@@ -1,15 +1,27 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Unpack
+from typing import override, TYPE_CHECKING, Any, Unpack
 
 from django.conf import settings
 from django.db.models import Model
 from modeltrans.utils import build_localized_fieldname
 from wagtail.admin.panels import FieldPanel, FieldRowPanel, MultiFieldPanel
 
+from kausal_common.i18n.helpers import convert_language_code, get_language_from_default_language_field
+
+
 if TYPE_CHECKING:
     from wagtail.admin.panels.field_panel import WidgetOverrideType
     from wagtail.admin.panels.group import PanelGroupInitArgs
+
+
+class PrimaryLanguagePanel(FieldPanel):
+    @override
+    def get_bound_panel(self, instance=None, request=None, form=None, prefix="panel"):
+        bound_panel = super().get_bound_panel(instance, request, form, prefix)
+        language = get_language_from_default_language_field(bound_panel.instance)
+        bound_panel.heading = f'{bound_panel.heading} ({language.upper()})'
+        return bound_panel
 
 
 class TranslatedLanguagePanel(FieldPanel):
@@ -19,7 +31,7 @@ class TranslatedLanguagePanel(FieldPanel):
     def __init__(self, field_name: str, language: str, **kwargs):
         self.main_field_name = field_name
         self.language = language
-        field_name = build_localized_fieldname(field_name, language.lower(), default_language='')
+        field_name = build_localized_fieldname(field_name, convert_language_code(language, 'modeltrans'), default_language='')
         super().__init__(field_name, **kwargs)
 
     def clone_kwargs(self):
@@ -28,24 +40,12 @@ class TranslatedLanguagePanel(FieldPanel):
         ret['language'] = self.language
         return ret
 
-    class BoundPanel(FieldPanel.BoundPanel):
-        panel: TranslatedLanguagePanel
-
-        def is_shown(self):
-            from paths.context import realm_context
-            instance = realm_context.get().realm
-            ret = super().is_shown()
-            if not ret:
-                return False
-            is_other_lang = self.panel.language in (instance.other_languages or [])
-            return is_other_lang
-
 
 class TranslatedFieldRowPanel[M: Model, P: Any](FieldRowPanel[M, P]):
     def __init__(self, field_name: str, widget: WidgetOverrideType | None = None, **kwargs: Unpack[PanelGroupInitArgs]):
         self.field_name = field_name
         self.widget = widget
-        primary_panel = FieldPanel(field_name, widget=widget, **kwargs)
+        primary_panel = PrimaryLanguagePanel(field_name, widget=widget, **kwargs)
         lang_panels = [TranslatedLanguagePanel(
             field_name=field_name,
             language=lang[0],
@@ -66,7 +66,7 @@ class TranslatedFieldPanel(MultiFieldPanel):
     def __init__(self, field_name: str, widget: Any = None, **kwargs):
         self.field_name = field_name
         self.widget = widget
-        primary_panel = FieldPanel(field_name, widget=widget, **kwargs)
+        primary_panel = PrimaryLanguagePanel(field_name, widget=widget, **kwargs)
         lang_panels = [TranslatedLanguagePanel(
             field_name=field_name,
             language=lang[0],
