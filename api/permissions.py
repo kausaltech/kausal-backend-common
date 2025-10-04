de901e12f896bd74ba33391841318d8e62ce8957
Timo Tuominen <tituomin@gmail.com>
2025-10-04 11:53:37 +0300

    Enforce requiring view permissions for list views too

--- api/permissions.py
+++ api/permissions.py
@@ -50,6 +50,16 @@ class PermissionPolicyDRFPermission[_M: PermissionedModel, CreateContext](  # py
         'DELETE': 'delete',
     }
 
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': ['%(app_label)s.view_%(model_name)s'],
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
     def __init__(self):
         self.permission_policy = self.Meta.model.permission_policy()
 
