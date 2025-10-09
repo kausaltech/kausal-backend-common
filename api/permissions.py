5ddfeaf628203a684e6231f341ae805458e361f2
Timo Tuominen <tituomin@gmail.com>
2025-10-09 18:53:15 +0300

    Add new permission class which handles nested permissions
    The goal is to give a 404 for everything after a
    non-accessible resource in a nested path

--- api/permissions.py
+++ api/permissions.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 from abc import ABCMeta, abstractmethod
 from typing import TYPE_CHECKING
 
+from django.db.models import ObjectDoesNotExist
 from rest_framework import permissions
+from rest_framework.exceptions import MethodNotAllowed, NotFound
 
 from kausal_common.models.permissions import PermissionedModel
 from kausal_common.users import user_or_none
@@ -23,12 +25,10 @@ class _MetaClass(permissions.BasePermissionMetaclass, ABCMeta):
 
 class PermissionModelMeta[_M: PermissionedModel]:
     model: type[_M]
+    allowed_actions: set[BaseObjectAction]
 
 
-class PermissionPolicyDRFPermission[_M: PermissionedModel, CreateContext](  # pyright: ignore[reportImplicitAbstractClass]
-    permissions.DjangoModelPermissions,
-    metaclass=_MetaClass
-):
+class PermissionPolicyDRFPermissionBase[_M: PermissionedModel, CreateContext](metaclass=_MetaClass):
     """
     A Django REST Framework Permission Class delegating to a Wagtail-style Permission Policy.
 
@@ -41,9 +41,11 @@ class PermissionPolicyDRFPermission[_M: PermissionedModel, CreateContext](  # py
 
     Meta: PermissionModelMeta[_M]
     permission_policy: ModelPermissionPolicy[_M, CreateContext]
+    allowed_actions: set[BaseObjectAction]
 
     HTTP_METHOD_TO_DJANGO_ACTION: dict[str, BaseObjectAction] = {
         'GET': 'view',
+        'HEAD': 'view',
         'POST': 'add',
         'PUT': 'change',
         'PATCH': 'change',
@@ -62,21 +64,15 @@ class PermissionPolicyDRFPermission[_M: PermissionedModel, CreateContext](  # py
 
     def __init__(self):
         self.permission_policy = self.Meta.model.permission_policy()
+        if not getattr(self.Meta, 'allowed_actions', False):
+            self.allowed_actions = {'view', 'add', 'change', 'delete'}
+        else:
+            self.allowed_actions = self.Meta.allowed_actions
 
-    def has_permission(self, request: Request, view: APIView):
-        if request.method is None:
-            return False
-        user = user_or_none(request.user)
-        if user is None:
-            return False
-        model_action = self.HTTP_METHOD_TO_DJANGO_ACTION[request.method]
-        if model_action == 'add':
-            context = self.get_create_context_from_api_view(view)
-            return self.permission_policy.user_can_create(user, context)
-        # Delegates to basic Django model permissions, which need to be set up for the user's role.
-        # After the model permissions return True, the has_object_permission method of this class
-        # will be called to check out the object-level permissions.
-        return super().has_permission(request, view)
+    def http_method_to_django_action(self, method: str | None) -> BaseObjectAction:
+        if method is None:
+            raise ValueError('Invalid method')
+        return self.HTTP_METHOD_TO_DJANGO_ACTION[method]
 
     def has_object_permission(self, request: Request, view, obj):
         if request.method is None:
@@ -90,3 +86,69 @@ class PermissionPolicyDRFPermission[_M: PermissionedModel, CreateContext](  # py
 
     @abstractmethod
     def get_create_context_from_api_view(self, view: APIView) -> CreateContext: ...
+
+
+class PermissionPolicyDRFPermission[_M: PermissionedModel, CreateContext](  # pyright: ignore[reportImplicitAbstractClass]
+    PermissionPolicyDRFPermissionBase[_M, CreateContext], permissions.DjangoModelPermissions,
+):
+    def has_permission(self, request: Request, view: APIView):
+        if request.method is None:
+            return False
+
+        user = user_or_none(request.user)
+        if user is None:
+            return False
+        model_action = self.http_method_to_django_action(request.method)
+        if model_action not in self.allowed_actions:
+            raise MethodNotAllowed(request.method)
+        if model_action == 'add':
+            try:
+                context = self.get_create_context_from_api_view(view)
+            except ObjectDoesNotExist as e:
+                raise NotFound() from e
+            return self.permission_policy.user_can_create(user, context)
+        # Delegates to basic Django model permissions, which need to be set up for the user's role.
+        # After the model permissions return True, the has_object_permission method of this class
+        # will be called to check out the object-level permissions.
+        return super().has_permission(request, view)
+
+
+class NestedPermissionModelMeta[_M: PermissionedModel, NestedParent: PermissionedModel](PermissionModelMeta[_M]):
+    view_kwargs_parent_key: str
+    nested_parent_key_field: str = 'uuid'
+    nested_parent_model: type[NestedParent]
+
+
+class NestedResourcePermissionPolicyDRFPermission[  # pyright: ignore[reportImplicitAbstractClass]
+    _M: PermissionedModel, CreateContext, NestedParent: PermissionedModel
+](
+    permissions.DjangoModelPermissions, PermissionPolicyDRFPermissionBase[_M, CreateContext], metaclass=_MetaClass
+):
+    Meta: NestedPermissionModelMeta[_M, NestedParent]
+
+    perms_map = PermissionPolicyDRFPermissionBase.perms_map
+
+    def get_nested_parent_from_api_view(self, view: APIView) -> NestedParent:
+        lookup_value = view.kwargs[self.Meta.view_kwargs_parent_key]
+        try:
+            object = self.Meta.nested_parent_model.objects.get(**{self.Meta.nested_parent_key_field: lookup_value})
+        except ObjectDoesNotExist as e:
+            raise NotFound() from e
+        return object
+
+    def has_permission(self, request: Request, view: APIView):
+        parent_obj = self.get_nested_parent_from_api_view(view)
+        pp = parent_obj.permission_policy()
+        user = user_or_none(request.user)
+        if not user:
+            return False
+        if request.method is None:
+            raise ValueError('No method supplied')
+        action = self.http_method_to_django_action(request.method)
+        if action not in self.allowed_actions:
+            raise MethodNotAllowed(request.method)
+        if not super().has_permission(request, view):
+            return False
+        if not pp.user_has_any_permission_for_instance(user, ['view'], parent_obj):
+            raise NotFound()
+        return True
