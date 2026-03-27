8ccf748ecc055288a90b1a3e5b140533e2d23f2a
Juha Yrjölä <juha.yrjola@iki.fi>
2026-03-27 09:59:53 +0200

    mypy-pre-push: Allow running without mypy baseline

--- development/git-hooks/mypy-pre-push.sh
+++ development/git-hooks/mypy-pre-push.sh
@@ -12,17 +12,21 @@ done < <(printf '%s\n' "$@" | "${SCRIPT_DIR}/filter-mypy-ignores.py")
 
 # Check if there are any files left after filtering
 if [ ${#filtered_files[@]} -gt 0 ]; then
-    # Run mypy on the filtered files
     set +e
-    mypy_output=$(mypy "${filtered_files[@]}" )
-    mypy_exit_code=$?
-    if [ $mypy_exit_code -ne 0 ] && [ $mypy_exit_code -ne 1 ]; then
-        echo "mypy crashed with exit code $mypy_exit_code. Exiting."
-        echo "$mypy_output"
-        exit $mypy_exit_code
+    if [ ! -f ".mypy-baseline.txt" ]; then
+        exec mypy "${filtered_files[@]}"
+    else
+        # Run mypy on the filtered files and compare against baseline
+        mypy_output=$(mypy "${filtered_files[@]}" )
+        mypy_exit_code=$?
+        if [ $mypy_exit_code -ne 0 ] && [ $mypy_exit_code -ne 1 ]; then
+            echo "mypy crashed with exit code $mypy_exit_code. Exiting."
+            echo "$mypy_output"
+            exit $mypy_exit_code
+        fi
+        set -e
+        echo "$mypy_output" | exec mypy-baseline filter --allow-unsynced --hide-stats
     fi
-    set -e
-    echo "$mypy_output" | exec mypy-baseline filter --allow-unsynced --hide-stats
 else
     echo "No files to check after filtering. Skipping mypy."
 fi
