716b995aa0e733d4cb8f0fb80288ddd9202378d5
Juha Yrjölä <juha.yrjola@iki.fi>
2026-03-28 18:10:28 +0200

    Add script to run the backend under portless

---
+++ development/tools/portless-backend.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$PORT" ]; then
+  echo "PORT is not set"
+  exit 1
+fi
+
+if [ -z "$PORTLESS_URL" ]; then
+  echo "PORTLESS_URL is not set"
+  exit 1
+fi
+
+export ADMIN_BASE_URL=$PORTLESS_URL
+
+exec python manage.py "$@" runserver 127.0.0.1:"$PORT"
