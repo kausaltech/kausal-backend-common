9e69e4f7787b74dae29718e60a1be635941177d4
Timo Tuominen <tituomin@gmail.com>
2026-06-08 15:17:13 +0300

    Add some maintenance commands to be run periodically
    The intention is to clean up the database from
    stale entries. Needs to be configured as a
    kubernetes cron job.

---
+++ docker/run-nightly-management-commands.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+python manage.py clearsessions
+python manage.py cleartokens
+# It's uncertain if we should run rebuild_references_index and when/how often
+# python manage.py rebuild_references_index
