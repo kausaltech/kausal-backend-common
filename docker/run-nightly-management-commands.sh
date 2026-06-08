79295b92b4be08579c4b6030c6c19ec151b685c1
Timo Tuominen <tituomin@gmail.com>
2026-06-08 16:15:29 +0300

    Run checks only once on cron jobs

--- docker/run-nightly-management-commands.sh
+++ docker/run-nightly-management-commands.sh
@@ -3,6 +3,6 @@
 set -e
 
 python manage.py clearsessions
-python manage.py cleartokens
+python manage.py cleartokens --skip-checks
 # It's uncertain if we should run rebuild_references_index and when/how often
 # python manage.py rebuild_references_index
