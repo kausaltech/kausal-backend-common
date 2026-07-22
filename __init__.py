4a28609ec867cc2048c351b1542569432ad847fd
Juha Yrjölä <juha.yrjola@iki.fi>
2026-07-22 18:28:36 +0300

    Configure jemalloc settings as early as possible to avoid memory leaks

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 from typing import Any, Literal
 
 from .mail.env import ENV_SCHEMA as MAIL_ENV_SCHEMA
@@ -18,4 +19,20 @@ def register_settings(settings: dict[str, Any]):
     init_debugger()
 
 
+_JEMALLOC_CONF = 'dirty_decay_ms:500,muzzy_decay_ms:0'
+
+
+def _early_init() -> None:
+    # This has to be set for polars; otherwise the process' RSS will grow indefinitely.
+    # Also, it has to be set before polars gets imported.
+    # See: https://github.com/pola-rs/polars/issues/23128
+    if old_val := os.getenv('_RJEM_MALLOC_CONF'):
+        if not old_val.endswith(_JEMALLOC_CONF):
+            raise RuntimeError(f'Invalid jemalloc configuration. Found: `{old_val}`)')
+    else:
+        os.environ['_RJEM_MALLOC_CONF'] = _JEMALLOC_CONF
+
+
 IS_MYPY: Literal[False] = False
+
+_early_init()
